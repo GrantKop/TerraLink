5f07a81a27335ad597c708a5cd7bdaa123fab5c2
Grant Kopczenski <grant.kop9@gmail.com>
2025-05-18 06:32:55 -0700

    Updated game version

--- include/core/game/Game.h
+++ include/core/game/Game.h
@@ -44,7 +44,7 @@ private:
     bool DEV_MODE = true;
     float gameVersionMajor = 0.f;
     float gameVersionMinor = 4.f;
-    float gameVersionPatch = 5.f;
+    float gameVersionPatch = 8.f;
 
     std::filesystem::path basePath = DEV_MODE
         ? std::filesystem::current_path().parent_path().parent_path()
